c2c1069c7f6564f4d20b14013289752d875c6333
Essenbay Assel <esenbay.asel@mail.ru>
2022-12-07 00:30:01 +0600

    step-17

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mynote/constants/routes.dart';
 import 'package:mynote/services/auth/auth_service.dart';
 import 'package:mynote/views/login_view.dart';
-import 'package:mynote/views/notes/new_notes_view.dart';
+import 'package:mynote/views/notes/create_update_notes_view.dart';
 import 'package:mynote/views/notes/notes_view.dart';
 import 'package:mynote/views/register_view.dart';
 import 'package:mynote/views/verify_email.dart';
@@ -21,7 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         myNoteRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOfUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
